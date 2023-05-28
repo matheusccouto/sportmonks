c6165d26b76d06b31cff26c86cd3fae8aae5d586
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2023-05-28 23:30:27 +0000

    Extract from api

---
+++ extract.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+url="https://api.sportmonks.com/v3/$ENDPOINT?per_page=50"
+
+while true; do
+
+  # Make the request using curl and retrieve the response
+  response=$(curl -s $url -H "Authorization: $SPORTMONKS_API_TOKEN")
+
+  # Check if the "message" key exists in the response
+  has_message=$(echo $response | jq -r 'has("message")')
+  if [ $has_message == true ]; then
+    error_message=$(echo $response | jq -r '.message')
+    echo Error: $error_message
+    exit 1
+  fi
+  
+  # Get the current page number from the response
+  page=$(echo $response | jq -r '.pagination.current_page')
+  echo Page $page
+
+  # Extract data from the response using jq and persist it
+  echo $response | jq -r '.data[]' > $page.json
+
+  # Check if there is a next page
+  url=$(echo $response | jq -r '.pagination.next_page')
+  if [ "$url" == null ]; then
+    break
+  fi
+
+done
